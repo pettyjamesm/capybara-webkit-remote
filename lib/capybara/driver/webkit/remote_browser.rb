1b173b72f7985c4fce6ec07de7da99c78e95bdc6
James Petty <jp@jibe.com>
2012-05-01 16:00:09 -0400

    Updated the capybara webkit at_exit hook to manage all open sockets together

--- lib/capybara/driver/webkit/remote_browser.rb
+++ lib/capybara/driver/webkit/remote_browser.rb
@@ -1,21 +1,34 @@
 class Capybara::Driver::Webkit
   class RemoteBrowser < Capybara::Driver::Webkit::Browser
     attr :remote_host, :remote_port
+    
+    def self.open_sockets; @@_sockets ||= [ ]; end
+    
+    at_exit do
+      socks = Capybara::Driver::Webkit::RemoteBrowser.open_sockets()
+      while(socks.length > 0)
+        s = socks.shift()
+        s.close() rescue nil
+      end
+    end
+    
     def initialize(hostname, port, options = { })
       @remote_host = hostname
       @remote_port = port
       super(options)
+      Capybara::Driver::Webkit::RemoteBrowser.open_sockets << @socket
     end
     
-    def disconnect!; @socket.close; end
+    def disconnect!
+      @socket.close rescue nil
+      Capybara::Driver::Webkit::RemoteBrowser.open_sockets.delete(@socket)
+      true
+    end
     
     def start_server ; end
     
     def attempt_connect
       @socket = @socket_class.open(remote_host, remote_port)
-      at_exit do
-        self.disconnect!
-      end
       rescue Errno::ECONNREFUSED
     end
   end 

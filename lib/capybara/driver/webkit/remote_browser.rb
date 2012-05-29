827b08c2331eeac4b26503646b0b6ea79dbbb1ee
James Petty <jp@jibe.com>
2012-05-29 19:45:52 -0400

    Rewrote capybara-webkit-remote to work with capybara 0.12.0

--- lib/capybara/driver/webkit/remote_browser.rb
+++ lib/capybara/driver/webkit/remote_browser.rb
@@ -1,35 +1,42 @@
 class Capybara::Driver::Webkit
-  class RemoteBrowser < Capybara::Driver::Webkit::Browser
-    attr :remote_host, :remote_port
-    
+  class RemoteConnection < Capybara::Driver::Webkit::Connection
     def self.open_sockets; @@_sockets ||= [ ]; end
-    
     at_exit do
-      socks = Capybara::Driver::Webkit::RemoteBrowser.open_sockets()
+      socks = Capybara::Driver::Webkit::RemoteConnection.open_sockets()
       while(socks.length > 0)
         s = socks.shift()
         s.close() rescue nil
       end
     end
+    def initialize(remote_host, remote_port, options)
+      @remote_host = remote_host
+      @remote_port = remote_port
+      super(options)
+      Capybara::Driver::Webkit::RemoteConnection.open_sockets << @socket
+    end
+    def close
+      @socket.close rescue nil
+      Capybara::Driver::Webkit::RemoteConnection.open_sockets.delete(@socket)
+      true
+    end
+    def start_server ; end
+    def attempt_connect
+      @socket = @socket_class.open(@remote_host, @remote_port)
+      rescue Errno::ECONNREFUSED
+    end
+  end
+  
+  class RemoteBrowser < Capybara::Driver::Webkit::Browser
+    attr :remote_host, :remote_port
     
     def initialize(hostname, port, options = { })
-      @remote_host = hostname
-      @remote_port = port
-      super(options)
-      Capybara::Driver::Webkit::RemoteBrowser.open_sockets << @socket
+      @connection = Capybara::Driver::Webkit::RemoteConnection.new(hostname, port, options)
+      super(@connection)
     end
     
     def disconnect!
-      @socket.close rescue nil
-      Capybara::Driver::Webkit::RemoteBrowser.open_sockets.delete(@socket)
+      @connection.close rescue nil
       true
-    end
-    
-    def start_server ; end
-    
-    def attempt_connect
-      @socket = @socket_class.open(remote_host, remote_port)
-      rescue Errno::ECONNREFUSED
     end
   end 
 end
